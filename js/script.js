ed3f2efd788e1cf86b9bed1375e9863cfbebb248
jorgelinaju <149181087+jorgelinaju@users.noreply.github.com>
2024-11-08 20:17:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
+// Seleccionamos los elementos necesarios
 let navbar = document.querySelector('.navbar');
+let searchForm = document.querySelector('.search-form');
+let cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('#menu-btn').onclick = () =>{
+// Control del botón de menú
+document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
-}
+};
 
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
+// Control del botón de búsqueda
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
-}
+};
 
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
+// Control del botón de carrito
+document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
-}
+};
 
-window.onscroll = () =>{
+// Cerrar todos los menús al hacer scroll
+window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
-}
+};
