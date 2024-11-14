24a62af08f707287c6234cd3060e64a76fbba10a
jorgelinaju <149181087+jorgelinaju@users.noreply.github.com>
2024-11-14 18:18:34 -0300

    c

--- js/validaciones.js
+++ js/validaciones.js
@@ -2,16 +2,15 @@
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
 const passwordRegex = /^\d{4,6}$/;
 
-// Selección del formulario y de los campos de email y contraseña
+// Selección de elementos
 const form = document.getElementById("login");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const errorMessage = document.getElementById("error-message");
+const inicioBtn = document.getElementById("inicioBtn");
 
-// Manejo del evento de envío del formulario
-form.onsubmit = (e) => {
-  e.preventDefault(); // Evita que el formulario se envíe inmediatamente
-
+// Manejo del evento de clic en el botón de inicio
+inicioBtn.onclick = () => {
   const email = emailInput.value;
   const password = passwordInput.value;
 
@@ -29,5 +28,6 @@ form.onsubmit = (e) => {
   }
 
   // Redirige a la página deseada si la autenticación es correcta
+  errorMessage.style.display = "none";  // Oculta el mensaje de error
   window.location.href = "/mostrar_mascotas.html";
 };
